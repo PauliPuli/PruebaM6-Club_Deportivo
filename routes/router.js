fdd3d0e495fe262e87f57f6d3dc88c4a95c7dfe6
Paulina San Martin <puli.sanmartin@gmail.com>
2024-04-14 19:19:19 -0400

    :star:Ruta /agregar enviando información a Deportes.Json

--- routes/router.js
+++ routes/router.js
@@ -1,33 +1,38 @@
-import express, { query } from "express";
+import express from "express";
 import fs from "fs";
-import path from 'path';
+import path from "path";
 const router = express.Router();
 const __dirname = import.meta.dirname;
 
-
-const deportes=[];
-const nombre=
-
 //Ruta raiz
-router.get("/", (req,res)=>{
-    res.sendFile(path.join(__dirname, "../views/index.html"))
-})
+router.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "../views/index.html"));
+});
 
 //Ruta que recibe nombre y precio
 router.get("/agregar", (req, res) => {
-    const { nombre, precio } = req.query;
-    const deporte = {
-        nombre,
-        precio, 
-      };  
-    const { data } = JSON.parse(fs.readFileSync("Deportes.json", "utf8")); 
-    const deportes = data.deporte;
+  const { nombre, precio } = req.query;
+  const deporte= {nombre, precio};
+  const postData= (deporte)=>{
+    let deportes = [];//aquí inserta los objetos al json
 
-    deportes.push(deporte); 
-    fs.writeFileSync("Deportes.json", JSON.stringify({ deportes })); 
-    console.log(data)
-    res.json(data);
-    return
-  });
+    try {
+        const data = fs.readFileSync("Deportes.json", "utf8");
+        const deporteData = JSON.parse(data);
+        if (deporteData.deportes) {
+          deportes = deporteData.deportes;
+        } 
+        deportes.push(deporte);
+        fs.writeFileSync("Deportes.json", JSON.stringify({ deportes }));
+        console.log(`${deporte.nombre} agregado a la lista`)
+      } catch (error) {
+        console.log("No se pudo agregar a Deportes.json: " + error.message);
+      }
+      return postData
+};
+res.send(`${deporte.nombre} agregado a la lista`);
+postData(deporte)
+});
 
-  export default router;
+
+export default router;
